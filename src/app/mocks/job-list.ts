f3a0e283932bcf65b1d41b98fa01b27b39dbda8f
Morhaf Shamia <inmogr@msn.com>
2020-05-26 13:07:45 +0800

    added mock data for job listing

---
+++ src/app/mocks/job-list.ts
@@ -0,0 +1,10 @@
+export const JobListData = [];
+
+for (let count = 1; count < 10; count++) {
+    JobListData.push({
+        title: `Title ${count}`,
+        companyName: `Company Name ${count}`,
+        companyLogo: "https://pngimage.net/wp-content/uploads/2018/06/logo-placeholder-png.png",
+        location: "Location City, Country",
+    });
+}
