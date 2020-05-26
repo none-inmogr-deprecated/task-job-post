08f8eb4422cd8ce170f32c19d15f1b74dcc5543f
Morhaf Shamia <inmogr@msn.com>
2020-05-26 12:17:06 +0800

    Initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+
+import {
+    PerfectScrollbarModule,
+    PERFECT_SCROLLBAR_CONFIG,
+    PerfectScrollbarConfigInterface,
+} from "ngx-perfect-scrollbar";
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+    suppressScrollX: true,
+};
+
+@NgModule(
+    {
+        declarations: [
+            AppComponent,
+        ],
+        imports: [
+            BrowserModule,
+            AppRoutingModule,
+            BrowserAnimationsModule,
+            ServiceWorkerModule.register(
+                "ngsw-worker.js",
+                {
+                    enabled:
+                        environment.production,
+                },
+            ),
+            PerfectScrollbarModule,
+        ],
+        providers: [
+            {
+                provide: PERFECT_SCROLLBAR_CONFIG,
+                useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG,
+            },
+        ],
+        bootstrap: [
+            AppComponent,
+        ],
+    },
+)
+export class AppModule {}
